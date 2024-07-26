6fb7e3d04ae691d3c632df4cbd91f38a39f3ead6
geovgy <gbh218@gmail.com>
2024-07-25 21:56:44 -0400

    Update env variables

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,13 @@
 import NextAuth from "next-auth"
+
+const {
+  AUTH_ALIAS_URL,
+  AUTH_ALIAS_ID,
+  AUTH_ALIAS_SECRET,
+  AUTH_REDIRECT_PROXY_URL
+} = process.env
+
+const redirectBaseUrl = AUTH_REDIRECT_PROXY_URL || "http://localhost:4000"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   // debug: true,
@@ -6,20 +15,20 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     id: "alias", // signIn("alias") and will be part of the callback URL
     name: "Alias", // optional, used on the default login page as the button text.
     type: "oauth", // "oidc" for Open ID or "oauth" for OAuth 2 providers
-    issuer: process.env.AUTH_ALIAS_URL, // to infer the .well-known/openid-configuration URL
+    issuer: AUTH_ALIAS_URL, // to infer the .well-known/openid-configuration URL
     authorization: {
-      url: `${process.env.AUTH_ALIAS_URL}/oauth2/auth`,
+      url: `${AUTH_ALIAS_URL}/oauth2/auth`,
       params: {
         scope: "offline profile", // include "openid" if using "oidc"
-        redirect_uri: `http://localhost:4000/api/auth/callback/alias`
+        redirect_uri: `${redirectBaseUrl}/api/auth/callback/alias`
       }
     }, //
-    clientId: process.env.AUTH_CLIENT_ID, // from the provider's dashboard
-    clientSecret: process.env.AUTH_CLIENT_SECRET, // from the provider's dashboard
+    clientId: AUTH_ALIAS_ID, // from the provider's dashboard
+    clientSecret: AUTH_ALIAS_SECRET, // from the provider's dashboard
     checks: ["pkce", "state"], // checks required by the provider
-    wellKnown: `${process.env.AUTH_ALIAS_URL}/.well-known/openid-configuration`, // to get the issuer, authorization, token, and userinfo URLs
-    token: {url: `${process.env.AUTH_ALIAS_URL}/oauth2/token`}, // to get the access_token along with the id_token 
-    userinfo: {url: `${process.env.AUTH_ALIAS_URL}/userinfo`}, // to get the user profile
+    wellKnown: `${AUTH_ALIAS_URL}/.well-known/openid-configuration`, // to get the issuer, authorization, token, and userinfo URLs
+    token: {url: `${AUTH_ALIAS_URL}/oauth2/token`}, // to get the access_token along with the id_token 
+    userinfo: {url: `${AUTH_ALIAS_URL}/userinfo`}, // to get the user profile
     profile(profile: any, tokens: any) {
       const token = tokens?.access_token || tokens?.id_token
       return {
